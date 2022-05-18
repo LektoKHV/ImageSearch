7994e4ff7a9b0b2cb38185d2b18af406ad8d2204
Lekto-Ideapad320 <krutvld@gmail.com>
2022-05-18 18:23:41 +0700

    Api key has been taken out to the constant value.

--- data/src/main/java/ru/vldkrt/imagesearch/data/api/ImageClient.kt
+++ data/src/main/java/ru/vldkrt/imagesearch/data/api/ImageClient.kt
@@ -6,13 +6,14 @@ import retrofit2.http.Query
 import ru.vldkrt.imagesearch.data.entities.ImageResultDataRemote
 
 const val LOAD_SIZE = 100
+const val API_KEY = "ea17e7f4915c83c603ed6a84f20eb40e29c9662c5a650f9580843332e15da1f0"
 
 interface ImageClient {
 
     @GET("search.json")
     suspend fun getImages(
         @Query("engine") engine: String = "google",
-        @Query("api_key") apiKey: String = "ea17e7f4915c83c603ed6a84f20eb40e29c9662c5a650f9580843332e15da1f0",
+        @Query("api_key") apiKey: String = API_KEY,
         @Query("tbm") tbm: String = "isch",
         @Query("device") device: String = "mobile",
 //        @Query("num") pageSize: Int = LOAD_SIZE,
